b501dba4a4839b6e12d85ae02dc275451e62c92c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-18 21:40:22 -0600

    adds /article index view

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -8,10 +8,10 @@ describe "user sees all articles" do
 
       visit '/articles'
 
-      expect(page).to have_content(article1.title)
-      expect(page).to have_content(article2.title)
-      expect(page).to have_content(article1.body)
-      expect(page).to have_content(article2.body)
+      expect(page).to have_content(article_1.title)
+      expect(page).to have_content(article_2.title)
+      expect(page).to have_content(article_1.body)
+      expect(page).to have_content(article_2.body)
     end
   end
 end
