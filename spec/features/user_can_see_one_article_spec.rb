1e4df743ecdda5cbb2520c8a561183f3e53ace21
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-21 13:47:20 -0600

    adds comment test

--- spec/features/user_can_see_one_article_spec.rb
+++ spec/features/user_can_see_one_article_spec.rb
@@ -4,13 +4,18 @@ describe "As a user" do
   context "When I visit the articles index path" do
     it "I can see the individual article" do
       article_1 = Article.create!(title: "title", body: "Lorem ipsum")
+      comment_1 = article_1.comments.create!(author_name: "Me", body: "Commentssss")
+      comment_2 = article_1.comments.create!(author_name: "You", body: "So much things to say wooowww")
 
-      visit '/articles'
+      visit articles_path
 
       click_link(article_1.title)
 
       expect(page).to have_content(article_1.title)
+      expect(page).to have_content(comment_1.author_name)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
