f568763173f8913c4926256e7c75500d381c2fea
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-20 19:45:05 -0600

    adds test to create article

---
+++ spec/features/user_can_create_an_article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "As a user" do
+  it "I can create a new article" do
+
+    visit articles_path
+    click_link "Create a New Article"
+
+    expect(current_path).to eq(new_article_path)
+
+    fill_in "article[title]", with: "New Title!"
+    fill_in "article[body]", with: "Some stuff"
+    click_on "Create Article"
+
+    expect(page).to have_content("New Title!")
+  end
+end
