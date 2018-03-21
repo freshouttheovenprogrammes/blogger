796ceb4750aeac95185bc34db304c3a6e866a8f2
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-21 15:21:03 -0600

    adds more comment functionality and cleans up comment section

--- spec/features/user_can_see_one_article_spec.rb
+++ spec/features/user_can_see_one_article_spec.rb
@@ -16,6 +16,7 @@ describe "As a user" do
       expect(page).to have_content(article_1.body)
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
+      expect(page).to have_content("2")
     end
   end
 end
