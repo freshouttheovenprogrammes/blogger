8c570a6c8f97553d7b21ff5f30a1e79dafbf4ef9
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-21 08:40:16 -0600

    adds more robust details on delete test

--- spec/features/user_can_delete_article_spec.rb
+++ spec/features/user_can_delete_article_spec.rb
@@ -1,20 +1,26 @@
 require "rails_helper"
 
 describe "Deleting an article" do
-  it "from the show page" do
-    article = Article.create!(title: "Title1", body: "Body1")
+  context "from the show page" do
+    it "displays all articles without the deleted entry" do
 
-    visit article_path(article)
+    article1 = Article.create!(title: "Title1", body: "Body1")
+    article1 = Article.create!(title: "Title2", body: "Body2")
+
+    visit article_path(article1)
 
     expect(page).to have_content "Title1"
     expect(page).to have_content "Body1"
-    expect(page).to have_link "Delete Article"
+    expect(page).to have_link "Delete"
 
     click_link "Delete Article"
 
     expect(page).to_not have_content "Title1"
     expect(page).to_not have_content "Body1"
-    
+    expect(page).to have_content "Title2"
+    expect(page).to have_content "Body2"
+
     expect(current_path).to eq articles_path
+    end
   end
 end
