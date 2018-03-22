35807e6196435f4c8756a7b8963e14a135802074
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-21 21:30:38 -0600

    my own interpretation of tag show test

---
+++ spec/features/user_can_see_a_single_tag_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "When I'm at the article show page" do
+  context "and I click on an articles tag" do
+    it "I arrive at the tag show" do
+      tag = Tag.create!(name: "Generic")
+      article1 = Article.create!(title: "Article1", body: "Article1 Body", tag_list: "Generic")
+      article2 = Article.create!(title: "Article2", body: "Article2 Body", tag_list: "Generic")
+
+      visit article_path(article1)
+
+      click_on "Generic"
+
+      expect(current_path).to eq tag_path(tag)
+      expect(page).to have_content(article1.title)
+      expect(page).to have_content(article2.title)
+    end
+  end
+end
