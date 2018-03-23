86128ed22ccda1da926abca07aafa47fca7a677f
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-23 14:22:28 -0600

    moving on, screw this comma seperation madness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def comma_seperated_links(list)
-    return if list.count == 0
-
-    list.collect do |item|
-      link_to(item.name, url_for((item)))
-    end.join(", ")
-
-  end
 end
--- spec/features/user_can_create_an_article_spec.rb
+++ spec/features/user_can_create_an_article_spec.rb
@@ -10,28 +10,11 @@ describe "As a user" do
 
     fill_in "article[title]", with: "New Title!"
     fill_in "article[body]", with: "Some stuff"
-    fill_in "article[tag_list]", with: "ruby"
-    click_on "Create Article"
-
-    expect(page).to have_content "Article: New Title! was created"
-    expect(page).to have_content("New Title!")
-    expect(page).to_not have_content("ruby,")
-    expect(page).to have_content("ruby")
-  end
-  it "multiple tags are comma seperated" do
-
-    visit articles_path
-    click_link "Create a New Article"
-
-    expect(current_path).to eq(new_article_path)
-
-    fill_in "article[title]", with: "New Title!"
-    fill_in "article[body]", with: "Some stuff"
     fill_in "article[tag_list]", with: "ruby, technology"
     click_on "Create Article"
-    save_and_open_page
+
     expect(page).to have_content "Article: New Title! was created"
     expect(page).to have_content("New Title!")
-    expect(page).to have_content("ruby, technology")
+    expect(page).to have_content("ruby technology")
   end
 end
